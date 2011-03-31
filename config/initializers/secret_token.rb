600ba08a4a6dc07c961ac634927d288620ef191d
Prakriti <itirkarp@gmail.com>
2011-03-31 11:50:25 +0530

    app structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TableReservation::Application.config.secret_token = '96c8eb6d8ef4c2bab7df1aaba88961144caedf98568ed2463140b74a53da5ddd896939e776a9250385ae584cfecc2e76a873490b76320e20a7e7ef37f7fe6b86'
