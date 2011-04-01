80650c4e6d6a7ba5229a6517389593b4813a9a32
Prakriti <itirkarp@gmail.com>
2011-04-01 12:46:09 +0530

    modified specs. added describe blocks

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -4,19 +4,19 @@ describe SessionsController do
   fixtures :all
   render_views
 
-  it "new action should render new template" do
+  it "should render new template" do
     get :new
     response.should render_template(:new)
   end
 
-  it "create action should render new template when authentication is invalid" do
+  it "should render new template when authentication is invalid" do
     User.stubs(:authenticate).returns(nil)
     post :create
     response.should render_template(:new)
     session['user_id'].should be_nil
   end
 
-  it "create action should redirect when authentication is valid" do
+  it "should redirect to home when authentication is valid" do
     User.stubs(:authenticate).returns(User.first)
     post :create
     response.should redirect_to(root_url)
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -4,55 +4,72 @@ describe UsersController do
   fixtures :all
   render_views
 
-  it "new action should render new template" do
-    get :new
-    response.should render_template(:new)
+  describe "new" do
+
+    it "should render new template" do
+      get :new
+      response.should render_template(:new)
+    end
+
   end
 
-  it "create action should render new template when model is invalid" do
-    User.any_instance.stubs(:valid?).returns(false)
-    post :create
-    response.should render_template(:new)
+  describe "create" do
+    it "should render new template when model is invalid" do
+      User.any_instance.stubs(:valid?).returns(false)
+      post :create
+      response.should render_template(:new)
+    end
+
+    it "should redirect when model is valid" do
+      User.any_instance.stubs(:valid?).returns(true)
+      post :create
+      response.should redirect_to(root_url)
+      session['user_id'].should == assigns['user'].id
+    end
+
   end
 
-  it "create action should redirect when model is valid" do
-    User.any_instance.stubs(:valid?).returns(true)
-    post :create
-    response.should redirect_to(root_url)
-    session['user_id'].should == assigns['user'].id
+  describe "edit" do
+    it "should redirect when not logged in" do
+      get :edit, :id => "ignored"
+      response.should redirect_to(login_url)
+    end
+
+    it "should render edit template" do
+      @controller.stubs(:current_user).returns(User.first)
+      get :edit, :id => "ignored"
+      response.should render_template(:edit)
+    end
+
   end
 
-  it "edit action should redirect when not logged in" do
-    get :edit, :id => "ignored"
-    response.should redirect_to(login_url)
-  end
+  describe "update" do
+    it "should redirect when not logged in" do
+      put :update, :id => "ignored"
+      response.should redirect_to(login_url)
+    end
 
-  it "edit action should render edit template" do
-    @controller.stubs(:current_user).returns(User.first)
-    get :edit, :id => "ignored"
-    response.should render_template(:edit)
-  end
+    it "should render edit template when user is invalid" do
+      @controller.stubs(:current_user).returns(User.first)
+      User.any_instance.stubs(:valid?).returns(false)
+      put :update, :id => "ignored"
+      response.should render_template(:edit)
+    end
 
-  it "update action should redirect when not logged in" do
-    put :update, :id => "ignored"
-    response.should redirect_to(login_url)
-  end
+    it "should redirect when user is valid" do
+      @controller.stubs(:current_user).returns(User.first)
+      User.any_instance.stubs(:valid?).returns(true)
+      put :update, :id => "ignored"
+      response.should redirect_to(root_url)
+    end
 
-  it "update action should render edit template when user is invalid" do
-    @controller.stubs(:current_user).returns(User.first)
-    User.any_instance.stubs(:valid?).returns(false)
-    put :update, :id => "ignored"
-    response.should render_template(:edit)
-  end
-
-  it "update action should redirect when user is valid" do
-    @controller.stubs(:current_user).returns(User.first)
-    User.any_instance.stubs(:valid?).returns(true)
-    put :update, :id => "ignored"
-    response.should redirect_to(root_url)
   end
 
   describe "show" do
-    it "should make the user details available"
+    it "should make the user details available" do
+      controller.stub(:current_user).and_return(User.new(:username => "name"))
+      get :show
+      assigns[:user].username.should == "name"
+    end
   end
 end
