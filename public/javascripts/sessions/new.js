7934e9d7ff6f4c9ee3c9a4032f742839a7734ed1
Prakriti <itirkarp@gmail.com>
2011-04-06 14:10:08 +0530

    UI changes on login page. footer, logo, tagline

--- public/javascripts/sessions/new.js
+++ public/javascripts/sessions/new.js
@@ -1,12 +1,16 @@
 (function($) {
     $(function() {
         $("#login").hover(function(){
-            $("#signup").addClass("dull");
-            $("#login").removeClass("dull");
+            $("#signup").removeClass("less-dull");
+            $("#signup").addClass("more-dull");
+            $("#login").removeClass("more-dull");
+            $("#login").addClass("less-dull");
         });
         $("#signup").hover(function(){
-            $("#login").addClass("dull");
-            $("#signup").removeClass("dull");
+            $("#login").removeClass("less-dull");
+            $("#login").addClass("more-dull");
+            $("#signup").removeClass("more-dull");
+            $("#signup").addClass("less-dull");
         });
     });
 })(jQuery);
