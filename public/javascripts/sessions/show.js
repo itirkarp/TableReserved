c8b0420640b4d225c189f2ea99a950f462ce8608
Prakriti <itirkarp@gmail.com>
2011-04-12 22:54:43 +0530

    tabs now done

--- public/javascripts/sessions/show.js
+++ public/javascripts/sessions/show.js
@@ -1,13 +1,5 @@
 $(document).ready(function() {
-    $('#tabs div').hide();
-    $('#tabs div:first').show();
-    $('#tabs ul li:first').addClass('active');
-    $('#tabs ul li a').click(function() {
-        $('#tabs ul li').removeClass('active');
-        $(this).parent().addClass('active');
-        var currentTab = $(this).attr('href');
-        $('#tabs div').hide();
-        $(currentTab).show();
-        return false;
+    $(function() {
+        $("#tabs").tabs();
     });
 });
