6798075ddd076eb0279288b7b261b4dbd2d92c72
Prakriti <itirkarp@gmail.com>
2011-04-13 00:46:14 +0530

    changed arrow. js to show cities

--- public/javascripts/sessions/show.js
+++ public/javascripts/sessions/show.js
@@ -2,4 +2,12 @@ $(document).ready(function() {
     $(function() {
         $("#tabs").tabs();
     });
+
+    $("#arrow").click(function() {
+        var citiesDropDown = $(this).find(".cities");
+        if (citiesDropDown.hasClass('hidden'))
+            citiesDropDown.removeClass('hidden');
+        else
+            citiesDropDown.addClass('hidden');
+    });
 });
