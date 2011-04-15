636008d39dbdb06001582e4281c00337ae0ad24b
Prakriti <itirkarp@gmail.com>
2011-04-15 18:06:10 +0530

    extracting sass partials. restaurant show

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,6 +2,7 @@ class RestaurantsController < ApplicationController
   before_filter :login_required
 
   def show
+    @restaurant = Restaurant.find_by_id(params[:id])
   end
 
 end
--- spec/controllers/restaurants_controller_spec.rb
+++ spec/controllers/restaurants_controller_spec.rb
@@ -5,5 +5,11 @@ describe RestaurantsController do
 #  render_views
 
   describe "show" do
+    it "should provide restaurant details" do
+      @controller.stubs(:current_user).returns(User.first)
+      Restaurant.should_receive(:find_by_id).with(1).and_return(Restaurant.new(:name => "Awesome"))
+      get :show, :id => 1
+      assigns[:restaurant].name.should == "Awesome"
+    end
   end
 end
