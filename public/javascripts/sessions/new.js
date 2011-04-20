8afd4029df053c7d91652b29fcd40cc1b6374a45
Prakriti <itirkarp@gmail.com>
2011-04-20 13:26:29 +0530

    removed background image except for login page

--- public/javascripts/sessions/new.js
+++ public/javascripts/sessions/new.js
@@ -1,16 +1,16 @@
-(function($) {
-    $(function() {
-        $("#login").hover(function(){
-            $("#signup").removeClass("less-dull");
-            $("#signup").addClass("more-dull");
-            $("#login").removeClass("more-dull");
-            $("#login").addClass("less-dull");
-        });
-        $("#signup").hover(function(){
-            $("#login").removeClass("less-dull");
-            $("#login").addClass("more-dull");
-            $("#signup").removeClass("more-dull");
-            $("#signup").addClass("less-dull");
-        });
+$(document).ready(function() {
+    $('body').addClass('login-photo');
+
+    $("#login").hover(function() {
+        $("#signup").removeClass("less-dull");
+        $("#signup").addClass("more-dull");
+        $("#login").removeClass("more-dull");
+        $("#login").addClass("less-dull");
     });
-})(jQuery);
+    $("#signup").hover(function() {
+        $("#login").removeClass("less-dull");
+        $("#login").addClass("more-dull");
+        $("#signup").removeClass("more-dull");
+        $("#signup").addClass("less-dull");
+    });
+});
