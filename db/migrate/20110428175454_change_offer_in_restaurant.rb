213c33f6a0fc9396402f531a71cda784d0da0b5c
Prakriti <itirkarp@gmail.com>
2011-04-29 00:12:23 +0530

    stupid roundabout migrations for heroku

--- db/migrate/20110428175454_change_offer_in_restaurant.rb
+++
@@ -1,9 +0,0 @@
-class ChangeOfferInRestaurant < ActiveRecord::Migration
-  def self.up
-    change_column :restaurants, :offer, :integer
-  end
-
-  def self.down
-    change_column :restaurants, :offer, :string
-  end
-end
