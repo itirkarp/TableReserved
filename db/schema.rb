8943aefbb8961e950c93415404dc246e06a7f6c5
Prakriti <itirkarp@gmail.com>
2011-05-09 11:42:47 +0530

    add token to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110503055117) do
+ActiveRecord::Schema.define(:version => 20110509054206) do
 
   create_table "restaurants", :force => true do |t|
     t.string  "name"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(:version => 20110503055117) do
     t.string   "first_name"
     t.string   "last_name"
     t.string   "zip_code"
+    t.string   "remember_token"
   end
 
 end
