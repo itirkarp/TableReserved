600e9015663b38b99560bcb9cad4e4b01c30897a
Prakriti <itirkarp@gmail.com>
2011-05-19 16:23:51 +0530

    ui feedback

--- public/javascripts/header.js
+++ public/javascripts/header.js
@@ -2,4 +2,12 @@ $(document).ready(function() {
     $("#logo").click(function() {
         window.location.href = "/"
     });
+
+    $("#arrow").click(function() {
+        var citiesDropDown = $(this).find("#cities");
+        if (citiesDropDown.hasClass('hidden'))
+            citiesDropDown.removeClass('hidden');
+        else
+            citiesDropDown.addClass('hidden');
+    });
 });
--- public/javascripts/sessions/show.js
+++ public/javascripts/sessions/show.js
@@ -2,12 +2,4 @@ $(document).ready(function() {
     $(function() {
         $("#tabs").tabs();
     });
-
-    $("#arrow").click(function() {
-        var citiesDropDown = $(this).find("#cities");
-        if (citiesDropDown.hasClass('hidden'))
-            citiesDropDown.removeClass('hidden');
-        else
-            citiesDropDown.addClass('hidden');
-    });
 });
